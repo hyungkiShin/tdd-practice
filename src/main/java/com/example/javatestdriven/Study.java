ed3aa0f2c82647a22b86f968bf066c46b4b81df7
hyungkiShin <tkaqkeldk@nate.com>
2023-01-05 00:20:55 +0900

    feat: test 반복하기

--- src/main/java/com/example/javatestdriven/Study.java
+++ src/main/java/com/example/javatestdriven/Study.java
@@ -6,6 +6,8 @@ public class Study {
 
     private int limit;
 
+    private String name;
+
     public Study(int limit) {
         if (limit < 0) {
             throw new IllegalArgumentException("limit은 0보다 커야 한다.");
@@ -13,11 +15,29 @@ public class Study {
         this.limit = limit;
     }
 
+    public Study(final int limit, final String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
     public StudyStatus getStatus() {
         return status;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public int getLimit() {
         return limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
